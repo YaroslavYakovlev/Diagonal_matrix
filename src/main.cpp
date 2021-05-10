e4541c33940118f91c655303ab844fa27498514a
Yaroslav Yakovlev <ekb-buza@mail.ru>
2021-05-10 15:18:25 +0500

    решение

---
+++ src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+
+const int n = 4;
+
+void fillingArray(int (&arr)[n][n], int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){      
+      std::cin >> arr[i][j];
+    }
+  }
+}
+
+void print(int (&arr)[n][n], int n){
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){
+      std::cout << arr[i][j];
+    }
+    std::cout << std::endl;
+  }
+}
+
+int main(){
+  std::cout << "Diagonal_matrix" << std::endl;
+
+  int arr_1[n][n];
+  std::cout << "Matrix" << std::endl;
+  fillingArray(arr_1, n);
+  std::cout << std::endl;  
+  print(arr_1, n);
+  std::cout << std::endl;  
+
+  for(int i = 0; i < n; i++){
+    for(int j = 0; j < n; j++){    
+      if(arr_1[i][i] != arr_1[i][j])
+        arr_1[i][j] = 0;
+    }
+  }
+  print(arr_1, n);
+
+  return 0;
+}
